1648c4fd94ab8ebca58454ffe4af272089486c1c
Glitch (easy-lms) <none>
2020-07-30 11:00:34 +0000

    🚅🗺 Updated with Glitch

---
+++ data/vision-sensor.js
@@ -0,0 +1,21 @@
+let data = [
+  {
+    title: "Courses",
+    headings: [
+      {
+        subtitle: "What is Vision Sensor? (vol.1)",
+        description: "",
+        link: "https://www.easy-lms.com/what-is-vision-sensor-vol.1/course-50276"
+      },
+      {
+        subtitle: "Vol 2",
+        description: "Under Development",
+        link: ""
+      }
+    ],
+    disabled_subtitles: [
+      "Vol 2",
+      ""
+    ]
+  },
+];
